7ebaea0c64e98915d923bbff374c8ceca833ff17
hsimao <e087754958@gmail.com>
2020-10-27 15:12:48 +0800

    add: add get devices

--- public/mediaStream/js/client.js
+++ public/mediaStream/js/client.js
@@ -1,4 +1,7 @@
 const videoPlay = document.getElementById('player')
+const audioSource = document.getElementById('audioSource')
+const audioOutput = document.getElementById('audioOutput')
+const videoSource = document.getElementById('videoSource')
 
 if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
   console.log('getUserMedia is not supported!')
@@ -7,13 +10,33 @@ if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
     video: true,
     audio: true,
   }
-  navigator.mediaDevices.getUserMedia(constraints).then(gotMediaStream).catch(handleError)
+  navigator.mediaDevices.getUserMedia(constraints).then(gotMediaStream).then(gotDevices).catch(handleError)
 }
 
 function gotMediaStream(stream) {
   videoPlay.srcObject = stream
+  // 回傳 enumerateDevices Promise, 讓取得成功後可接續使用 .then 操作
+  return navigator.mediaDevices.enumerateDevices()
 }
 
 function handleError(err) {
   console.log(`getUserMedia err: ${err}`)
+}
+
+function gotDevices(deviceInfos) {
+  deviceInfos.forEach(function (deviceInfo) {
+    console.log(
+      `${deviceInfo.kind}: label = ${deviceInfo.label}: id = ${deviceInfo.deviceId}: groupId = ${deviceInfo.groupId}`
+    )
+    const option = document.createElement('option')
+    option.text = deviceInfo.label
+    option.value = deviceInfo.deviceId
+    if (deviceInfo.kind === 'audioinput') {
+      audioSource.appendChild(option)
+    } else if (deviceInfo.kind === 'audiooutput') {
+      audioOutput.appendChild(option)
+    } else if (deviceInfo.kind === 'videoinput') {
+      videoSource.appendChild(option)
+    }
+  })
 }
