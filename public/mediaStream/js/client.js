61dd0ac9dc9637d11ce382961b339a88aef50d73
hsimao <e087754958@gmail.com>
2020-10-28 20:48:03 +0800

    add: 錄影功能

--- public/mediaStream/js/client.js
+++ public/mediaStream/js/client.js
@@ -8,6 +8,13 @@ const snapshot = document.getElementById('snapshot')
 const picture = document.getElementById('picture')
 const divConstraints = document.getElementById('constraints')
 
+// recored 錄製
+const recPlayer = document.getElementById('recPlayer')
+const btnRecord = document.getElementById('record')
+const btnRecPlay = document.getElementById('recplay')
+const btnRecDownload = document.getElementById('download')
+let buffer, mediaRecorder
+
 function start() {
   if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
     console.log('getUserMedia is not supported!')
@@ -42,6 +49,7 @@ function gotMediaStream(stream) {
   const videoConstraints = videoTrack.getSettings()
   divConstraints.textContent = JSON.stringify(videoConstraints)
 
+  window.stream = stream
   // 回傳 enumerateDevices Promise, 讓取得成功後可接續使用 .then 操作
   return navigator.mediaDevices.enumerateDevices()
 }
@@ -84,4 +92,56 @@ pictureInit()
 snapshot.onclick = function () {
   picture.className = filtersSelect.value
   picture.getContext('2d').drawImage(videoPlay, 0, 0, picture.width, picture.height)
+}
+
+// Recored logic
+btnRecord.onclick = () => {
+  if (btnRecord.textContent === 'Start Record') {
+    startRecord()
+    btnRecord.textContent = 'Stop Record'
+    btnRecPlay.disabled = true
+    btnRecDownload.disabled = true
+  } else {
+    stopRecord()
+    btnRecord.textContent = 'Start Record'
+    btnRecPlay.disabled = false
+    btnRecDownload.disabled = false
+  }
+}
+
+btnRecPlay.onclick = () => {
+  const blob = new Blob(buffer, { type: 'video/webm' })
+  recPlayer.src = window.URL.createObjectURL(blob)
+  recPlayer.srcObject = null
+  recPlayer.controls = true
+  recPlayer.play()
+}
+
+function handleDataAvailable(e) {
+  if (e && e.data && e.data.size > 0) {
+    buffer.push(e.data)
+  }
+}
+
+function startRecord() {
+  buffer = []
+  const options = {
+    mimeType: 'video/webm;codecs=vp8',
+  }
+  if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+    console.error(`${options.mimeType} is not supported!`)
+    return
+  }
+  try {
+    mediaRecorder = new MediaRecorder(window.stream, options)
+    mediaRecorder.ondataavailable = handleDataAvailable
+    mediaRecorder.start(10)
+  } catch (e) {
+    console.error('Failed to create MediaRecorder: ', e)
+    return
+  }
+}
+
+function stopRecord() {
+  mediaRecorder.stop()
 }
